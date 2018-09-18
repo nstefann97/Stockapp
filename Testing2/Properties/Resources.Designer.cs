38d17e4fcef3db1cfd7c9bad9a809e049ef6b75a
Stefan Nastase <t-stnast@microsoft.com>
2018-09-18 15:45:06 +0300

    background photo 2.0

--- Testing2/Properties/Resources.Designer.cs
+++ Testing2/Properties/Resources.Designer.cs
@@ -99,5 +99,15 @@ namespace Testing2.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap PreviewImage__3_ {
+            get {
+                object obj = ResourceManager.GetObject("PreviewImage (3)", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
